52dcfc65ae96bbb89f8fd59c961cfa1882317f3c
ayworld <ayo.tim.fola@gmail.com>
2019-03-02 18:32:01 -0600

    final changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,29 @@
+# import dependencies
 import os
 import csv
 
 csvpath = os.path.join("..", "PyBank", "budget_data.csv")
-      
+
+# create lists to store files    
 months = []
 profit_losses = []
 average_change = []
 
+# Set path for file
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+# skip header
     header = next(csvreader)
    
-    print("Financial Analysis")
-    print("------------------------")
-
+    
+# iterate through rows to retrieve data to store in the empty lists above 
     for row in csvreader:
         months.append(row[0])
         profit_losses.append(int(row[1]))
     
-    print(f"Total Months: {len(months)}")
-    print(f"Total: {sum(profit_losses)}")
     
+  # run second loop for calculations  
     for i in range(1, len(profit_losses)):
         average_change.append(profit_losses[i] - profit_losses[i-1])
         pl_change = round(sum(average_change)/len(average_change), 2)
@@ -32,6 +34,22 @@ with open(csvpath, newline="") as csvfile:
         max_profit_date = str(months[average_change.index(max(average_change)) +1])
         min_profit_date = str(months[average_change.index(min(average_change)) +1])
 
+# print statements
+    print("Financial Analysis")
+    print("------------------------")
+    print(f"Total Months: {len(months)}")
+    print(f"Total: {sum(profit_losses)}")
     print(f"Average Change: $ {pl_change}")
     print(f"Greatest Increase in Profits: {max_profit_date} ({max_profit})")
     print(f"Greatest Decrease in Profits: {min_profit_date} ({min_profit})")
+
+# write print statements to text file
+f = open("PyBank.txt","w+")  
+f.write("Financial Analysis\n")
+f.write("------------------------\n")
+f.write(f"Total Months: {len(months)}\n")
+f.write(f"Total: {sum(profit_losses)}\n")
+f.write(f"Average Change: $ {pl_change}\n")
+f.write(f"Greatest Increase in Profits: {max_profit_date} ({max_profit})\n")
+f.write(f"Greatest Decrease in Profits: {min_profit_date} ({min_profit})\n")
+f.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,11 @@
+# import dependencies
 import os
 import csv
 
+# set path for file source
 csvpath = os.path.join("..", "PyPoll", "election_data.csv")
-      
+
+# create lists to store files        
 total_votes = []
 c_khan = []
 c_correy = []
@@ -10,17 +13,20 @@ c_li = []
 c_otooley = []
 average_change = []
 
+# open csv file
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+# skip header
     header = next(csvreader)
    
-    print("Election Results")
-    print("------------------------")
-
+    
+# iterate through rows to retrieve data to store in the empty lists above 
     for row in csvreader:
         total_votes.append(row[2])
 
+
+# tally up total votes for each candidates by looping over entire row and add to empty lists
         if row[2] == "Khan":
             c_khan.append(row[2])
         elif row[2] == "Correy":
@@ -30,11 +36,27 @@ with open(csvpath, newline="") as csvfile:
         elif row[2] == "O'Tooley":
             c_otooley.append(row[2])
 
+# convert each candidates votes to percentages of total votes
     khan_percent = round(len(c_khan)/len(total_votes) * 100, 3)
     correy_percent = round(len(c_correy)/len(total_votes) * 100, 3)
     li_percent = round(len(c_li)/len(total_votes) * 100, 3)
     otooley_percent = round(len(c_otooley)/len(total_votes) * 100, 3)
-    
+
+# get winner  
+    percent_list = [khan_percent, correy_percent, li_percent, otooley_percent]
+    maximum = max(percent_list)
+    if khan_percent == maximum:
+        winner = "Khan"
+    if correy_percent == maximum:
+        winner = "Correy"
+    if li_percent == maximum:
+        winner = "Li"
+    if otooley_percent == maximum:
+        winner = "O'Tooley"
+
+  # print statements  
+    print("Election Results")
+    print("------------------------")
     print(f"Total Votes: {len(total_votes)}")
     print("------------------------")    
     print(f"Khan: {khan_percent}% ({len(c_khan)})")
@@ -42,5 +64,20 @@ with open(csvpath, newline="") as csvfile:
     print(f"Li: {li_percent}% ({len(c_li)})")
     print(f"O'Tooley: {otooley_percent}% ({len(c_otooley)})")
     print("------------------------")
-    print(f"Winner: Khan")
+    print(f"Winner: {winner}")
     print("------------------------")
+
+# write print statements to text file
+f = open("PyPoll.txt", "w+")
+f.write("Election Results\n")
+f.write("------------------------\n")
+f.write(f"Total Votes: {len(total_votes)}\n")
+f.write("------------------------\n")    
+f.write(f"Khan: {khan_percent}% ({len(c_khan)})\n")
+f.write(f"Correy: {correy_percent}% ({len(c_correy)})\n")
+f.write(f"Li: {li_percent}% ({len(c_li)})\n")
+f.write(f"O'Tooley: {otooley_percent}% ({len(c_otooley)})\n")
+f.write("------------------------\n")
+f.write(f"Winner: {winner}\n")
+f.write("------------------------\n")
+f.close()
